bc70826bdb8232c83a29048c0e08a50683a67d73
Närhi Minna Johanna <minna.j.narhi@student.jyu.fi>
2024-08-30 08:28:14 +0300

    Tehtavia

---
+++ unicafe/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positivePercentage = (good / total) * 100
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
+      <h1>statistics</h1>
+      <Statistics 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+        total={total}
+        average={average}
+        positivePercentage={positivePercentage} 
+      />
+    </div>
+  )
+}
+
+const Statistics = ({ good, neutral, bad, total, average, positivePercentage }) => {
+  if (total === 0) {
+    return <div>No feedback given</div>
+  }
+
+  return (
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positivePercentage + " %"} />
+    </div>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <div>
+      {text}: {value}
+    </div>
+  )
+}
+
+export default App
